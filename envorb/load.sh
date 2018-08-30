500ff73f5195fcf9d4a83e7c87759630f364574f
Matthias Fax <matthias.fax@gmail.com>
2018-08-30 12:48:10 +0200

    Check for env store

--- envorb/load.sh
+++ envorb/load.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
-OLD_IFS=${IFS}
-IFS=';'
-for file in $(find ${PWD}/env -maxdepth 1 -iname '*.env' -exec basename {} .env ';' | tr '\n' ';'); do
-    export ${file}="$(cat ${PWD}/env/${file}.env)"
-    eval "echo \"${file}: \${${file}}\""
-done
-IFS=${OLD_IFS}
+if [ -d "${PWD}/env" ]; then
+    echo "Checking env store..."
+    OLD_IFS=${IFS}
+    IFS=';'
+    for file in $(find ${PWD}/env -maxdepth 1 -iname '*.env' -exec basename {} .env ';' | tr '\n' ';'); do
+        export ${file}="$(cat ${PWD}/env/${file}.env)"
+        eval "echo \"${file}: \${${file}}\""
+    done
+    IFS=${OLD_IFS}
+else
+    echo "SKIPPING..."
+fi
