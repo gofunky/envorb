f1c386d2dddbe94e56ee0530293681fd9e9da41e
matfax <matthias.fax@gmail.com>
2018-11-11 15:57:56 +0100

    Add git_tag and git_name commands to the orb

--- load.sh
+++ load.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-if [ -d "${PWD}/env" ]; then
+if [ -d "${PWD}/.env" ]; then
     echo "Checking env store..."
     OLD_IFS=${IFS}
     IFS=';'
-    for file in $(find ${PWD}/env -maxdepth 1 -iname '*.env' -exec basename {} .env ';' | tr '\n' ';'); do
-        export ${file}="$(cat ${PWD}/env/${file}.env)"
+    for file in $(find ${PWD}/.env -maxdepth 1 -iname '*.env' -exec basename {} .env ';' | tr '\n' ';'); do
+        export ${file}="$(cat ${PWD}/.env/${file}.env)"
         eval "echo \"${file}: \${${file}}\""
     done
     IFS=${OLD_IFS}
