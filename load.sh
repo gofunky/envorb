05c9c58d17a961cc39710f4a4492d88044356f3f
matfax <matthias.fax@gmail.com>
2018-11-11 22:26:38 +0100

    Fix env suffix

--- load.sh
+++ load.sh
@@ -3,8 +3,8 @@ if [ -d "${PWD}/.envs" ]; then
     echo "Checking env store..."
     OLD_IFS=${IFS}
     IFS=';'
-    for file in $(find ${PWD}/.envs -maxdepth 1 -iname '*.envs' -exec basename {} .envs ';' | tr '\n' ';'); do
-        export ${file}="$(cat ${PWD}/.envs/${file}.envs)"
+    for file in $(find ${PWD}/.envs -maxdepth 1 -iname '*.env' -exec basename {} .env ';' | tr '\n' ';'); do
+        export ${file}="$(cat ${PWD}/.envs/${file}.env)"
         eval "echo \"${file}: \${${file}}\""
     done
     IFS=${OLD_IFS}
